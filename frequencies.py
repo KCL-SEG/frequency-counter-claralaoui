e0ad9a5f113d5123178190d732a7d6fd98e078ca
Clara <clara.laoui@kcl.ac.uk>
2023-09-21 19:01:31 +0200

    fourth try

--- frequencies.py
+++ frequencies.py
@@ -1,11 +1,8 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
 def frequencies(items):
     frequencies = {}
     for item in items:
-        frequencies[str(item)] = items.count(str(item))
+        frequencies[str(item)] = items.count(item)
     return frequencies
 
-list = [5, 5, 5, 7 ,7 , 'ab', 'ab', 'c']
-frequencies(list)
